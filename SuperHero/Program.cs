8f32563c7a8aa2cf2533689c189df6f62629ade7
HanChengLiang <James_61111@live.ca>
2015-02-06 23:19:35 -0500

    added string array abd construtor

---
+++ SuperHero/Program.cs
@@ -0,0 +1,21 @@
+﻿// Author: Han Cheng Liang
+// ID: 300791705
+
+using System;
+
+namespace hero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            hero hero = new hero("Lux");
+            hero.show();
+            hero.fight();
+
+            SuperHero superHero = new SuperHero("BatMan");
+            superHero.showPowers();
+            superHero.fight();
+        }
+    }
+}
---
+++ SuperHero/SuperHero.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hero
+{
+    class SuperHero : hero
+    {
+
+        private string[] superPowers;
+
+        //constructor
+        public SuperHero(string name) :
+            base(name)
+        {
+            this.generateRandomPowers();
+        }
+
+      
+
+
+
+
+
+    }
+}
+
+
